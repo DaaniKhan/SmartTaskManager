92421fd043a203762bbcb3a99e595305e9d666a3
Daanish Khan <70915304+DaaniKhan@users.noreply.github.com>
2025-07-05 14:13:24 +0500

    Add Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
 
   return (
-    <>Hello</>
+    <div>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
